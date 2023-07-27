cb3b9a446271b9ac6d5651c4c5c7994afa3e1577
AhmetSIRIM <ahmet.sirim@outlook.com>
2023-07-28 01:15:29 +0300

    Add ArticleDao.kt

--- app/src/main/java/com/asirim/mvvmnewsappstudy/data/dto/Article.kt
+++ app/src/main/java/com/asirim/mvvmnewsappstudy/data/dto/Article.kt
@@ -1,23 +1,18 @@
 package com.asirim.mvvmnewsappstudy.data.dto
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-import com.google.gson.annotations.SerializedName
-
+@Entity("articles_table")
 data class Article(
-    @SerializedName("author")
+    @PrimaryKey(true)
+    var id: Int? = null,
     val author: String?,
-    @SerializedName("content")
     val content: String?,
-    @SerializedName("description")
     val description: String?,
-    @SerializedName("publishedAt")
     val publishedAt: String?,
-    @SerializedName("source")
     val source: Source?,
-    @SerializedName("title")
     val title: String?,
-    @SerializedName("url")
     val url: String?,
-    @SerializedName("urlToImage")
     val urlToImage: String?
 )
--- app/src/main/java/com/asirim/mvvmnewsappstudy/data/dto/Source.kt
+++ app/src/main/java/com/asirim/mvvmnewsappstudy/data/dto/Source.kt
@@ -1,11 +1,6 @@
 package com.asirim.mvvmnewsappstudy.data.dto
 
-
-import com.google.gson.annotations.SerializedName
-
 data class Source(
-    @SerializedName("id")
     val id: String?,
-    @SerializedName("name")
     val name: String?
 )
